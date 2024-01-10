79ce716b407e789522f8a2b7ea3724bc0acdee60
Hoai Phong <hoaiphong00509@gmail.com>
2024-01-10 19:07:08 +0700

    add client
    add client

---
+++ src/models/Client.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const { extend, BaseSchema } = require('./_base')
+
+const ClientSchema = extend(BaseSchema, {
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  vatNumber: {
+    type: String,
+    default: ''
+  },
+  taxNumber: {
+    type: Date,
+    default: null
+  },
+  phone: {
+    type: Date,
+    default: ''
+  },
+  avatar: {
+    type: String,
+    default: ''
+  },
+  shippingAddress: {
+    country: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    stateCode: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    zip: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    line1: {
+      type: String,
+      default: '',
+      trim: true
+    },
+    line2: {
+      type: String,
+      default: '',
+      trim: true
+    }
+  }
+})
+
+module.exports = mongoose.model('client', ClientSchema)
---
+++ src/services/client.service.js
@@ -0,0 +1,100 @@
+'use strict'
+
+const Client = require('../models/Client')
+const { BadRequestError, NotFoundError } = require('../helper/error.response')
+
+class ClientService {
+  static getPaginated = async ({
+    keyword = '',
+    page = 1,
+    limit = 12,
+    isDeleted = false,
+    isActive = true
+  }) => {
+    const categories = await Client.find({
+      $or: [
+        {
+          name: {
+            $regex: keyword,
+            $options: 'i'
+          }
+        }
+      ],
+      isDeleted,
+      isActive
+    })
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec()
+
+    const count = await Client.countDocuments({
+      isDeleted,
+      isActive
+    })
+
+    return {
+      page,
+      limit,
+      totalPages: Math.ceil(count / limit),
+      totalRecords: count,
+      currentRecords: categories.length,
+      records: categories
+    }
+  }
+
+  static getDetail = async (id) => {
+    const foundClient = await Client.findById(id)
+    if (!foundClient) {
+      throw new NotFoundError()
+    }
+    return foundClient
+  }
+
+  static create = async (payload) => {
+    const clientExist = await User.findOne({
+      email: payload.email,
+      isDeleted: false,
+      isActive: true
+    })
+
+    if (clientExist) {
+      throw new BadRequestError('Khách hàng với email này đã tồn tại.')
+    }
+
+    const newClient = new Client({
+      ...payload
+    })
+
+    return await newClient.save()
+  }
+
+  static update = async (payload) => {
+    const clientExit = await User.findOne({
+      email: payload.email,
+      isDeleted: false,
+      isActive: true
+    })
+
+    if (clientExit && clientExit._id !== payload.clientId) {
+      throw new BadRequestError('Khách hàng với email này đã tồn tại.')
+    }
+
+    const clientField = {
+      ...payload
+    }
+    delete clientField['clientId']
+
+    return await Client.findByIdAndUpdate(payload.clientId, payload, {
+      new: true
+    })
+  }
+
+  static delete = async (id) => {
+    await Client.findByIdAndUpdate(id, {
+      isDeleted: true
+    })
+    return {}
+  }
+}
+
+module.exports = ClientService
